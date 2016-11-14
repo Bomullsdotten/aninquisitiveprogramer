05aac5a32cbd8c0f0ba50b35d9e54efc88f38364
Bomullsdotten <sondre_erstad@hotmail.com>
2016-11-14 21:51:11 +0100

    removed commented code

--- Blog/blog/__init__.py
+++ Blog/blog/__init__.py
@@ -9,14 +9,5 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
-    """
-    authentication_policy = AuthTktAuthenticationPolicy(
-        settings['blog.secret'], callback=groupfinder,
-        hashalg='sha512'
-    )
-    authorization_policy = ACLAuthorizationPolicy()
-    config.set_authentication_policy(authentication_policy)
-    config.set_authorization_policy(authorization_policy)
-    """
     config.scan()
     return config.make_wsgi_app()
