ab72b0e453d1752694b9ad6ef9bc3c3926bcfbb7
Bomullsdotten <sondre_erstad@hotmail.com>
2016-11-15 21:07:08 +0100

    setup more views

--- Blog/blog/views/default.py
+++ Blog/blog/views/default.py
@@ -14,3 +14,31 @@ def home(request):
         project='Blog'
     )
 
+
+@view_config(route_name='blog_action', match_param='action=edit',
+             renderer='blog:templates/edit_blog.jinja2')
+def blog_update(request):
+    return {}
+
+
+@view_config(route_name='blog_action', match_param='action=create',
+             renderer='blog:templates/edit_blog.jinja2')
+def blog_create(request):
+    return {}
+
+
+@view_config(route_name='login', renderer='blog:templates/login.jinja2',
+             request_method='POST')
+def login(request):
+    return {}
+
+
+@view_config(route_name='logout', renderer='string')
+def logout(request):
+    return {}
+
+
+@view_config(renderer_name='blog', renderer='blog:templates/view_blog.jinja2')
+def view_blog(request):
+    return {}
+
