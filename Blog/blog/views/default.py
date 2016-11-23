031e6759c0d3f2fcb29b642bad5240f2257989b2
Bomullsdotten <sondre_erstad@hotmail.com>
2016-11-23 22:28:54 +0100

    added login logout functionality

--- Blog/blog/views/default.py
+++ Blog/blog/views/default.py
@@ -4,8 +4,14 @@ from pyramid.httpexceptions import (
     HTTPNotFound
 )
 
+from pyramid.httpexceptions import HTTPFound
+
+from pyramid.security import remember
+from pyramid.security import forget
+
 from ..models import User, BlogPost
 from ..actions.blog_post import BlogPostService
+from ..actions.user import UserQueries
 
 
 @view_config(route_name='home', renderer='blog:templates/index.jinja2')
@@ -21,10 +27,24 @@ def home(request):
 @view_config(route_name='login', renderer='blog:templates/login.jinja2',
              request_method=['GET', 'POST'])
 def login(request):
-    return {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        if username:
+            user = UserQueries.by_name(username, request=request)
+            if user and user.check_password(request.POST.get('password')):
+                headers = remember(request, user.name)
+            else:
+                headers =  forget(request)
+        else:
+            headers = forget(request)
+
+        return HTTPFound(location=request.route_url('home'), headers=headers)
+    elif request.method == 'GET':
+        return {}
 
 
 @view_config(route_name='logout', renderer='string')
 def logout(request):
-    return {}
+    headers = forget(request)
+    return HTTPFound(location=request.route_url('home'), headers=headers)
 
