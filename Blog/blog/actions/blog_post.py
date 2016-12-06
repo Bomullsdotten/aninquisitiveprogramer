0b66fbc6ef4cdad892a374a7a583fbbe0675060d
Sondre <sondre_erstad@hotmail.com>
2016-12-06 13:47:50 +0100

    quick fix

--- Blog/blog/actions/blog_post.py
+++ Blog/blog/actions/blog_post.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import sqlalchemy
-from ..models.blog_post import BlogPost
-from paginate_sqlalchemy import SqlalchemyOrmPage
-
-class BlogPostService(object):
-    @classmethod
-    def all(cls, request):
-        query = request.dbsession.query(BlogPost)
-        return query.order_by(sqlalchemy.desc(BlogPost.created))
-
-    @classmethod
-    def by_id(cls, _id, request):
-        query = request.dbsession.query(BlogPost)
-        return query.get(_id)
-
-    @classmethod
-    def get_paginator(cls, request, page=1):
-        try:
-            query = request.dbsession.query(BlogPost)
-            query = query.order_by(sqlalchemy.desc(BlogPost.created))
-            query_params = request.GET.mixed()
-
-            def url_maker(link_page):
-                query_params['page'] = link_page
-                return request.current_route_url(_query=query_params)
-
-            titels_per_page = 5
-            return SqlalchemyOrmPage(query, page, items_per_page=titels_per_page,
-                                     url_maker=url_maker)
-        except sqlalchemy.exc.OperationalError:
-            return None
-=======
 import sqlalchemy
 from ..models.blog_post import BlogPost
 from paginate_sqlalchemy import SqlalchemyOrmPage
@@ -57,6 +24,6 @@ class BlogPostService(object):
             query_params['page'] = link_page
             return request.current_route_url(_query=query_params)
 
-        titels_per_page = 5
-        return SqlalchemyOrmPage(query, page, items_per_page=titels_per_page,
+        titles_per_page = 5
+        return SqlalchemyOrmPage(query, page, items_per_page=titles_per_page,
                                  url_maker=url_maker)
